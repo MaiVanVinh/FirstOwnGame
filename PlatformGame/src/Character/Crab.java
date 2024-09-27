98dd92f9b465ae5de7d0262b277de4b83e32d29f
MaiVanVinh <maivinhscj2601@gmail.com>
2024-09-27 11:51:20 +0700

    Update Crab.java
    Add First Check to run the animation of the enemy

--- PlatformGame/src/Character/Crab.java
+++ PlatformGame/src/Character/Crab.java
@@ -8,6 +8,7 @@ public class Crab extends Enemy {
 	public static final int CRABBY_HEIGHT_DEFAULT = 32;
 	public static final int CRABBY_WIDTH = (int) (CRABBY_WIDTH_DEFAULT * MainGame.SCALE);
 	public static final int CRABBY_HEIGHT = (int) (CRABBY_HEIGHT_DEFAULT * MainGame.SCALE);
+	public int firstCheck = 0;
 
 	public Crab(float x, float y) {
 		super(x + 30 ,y + 5 , CRABBY_WIDTH - 57, CRABBY_HEIGHT-10);
